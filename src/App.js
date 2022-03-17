4a4ceb901e1f04b458f86e086f2be6c42ddea2d9
codessi <johansuh@gmail.com>
2022-03-17 15:19:49 -0700

    interative section complete, responsive

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 
 import './App.scss'
 import Header from './components/Header';
+import Interactive from './components/Interactive';
+import Creation from './components/Creation';
 
 function App() {
 
   return (
     <>
       <Header />
+      <Interactive />
+      <Creation />
     </>
   );
 }
---
+++ src/components/Interactive.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import vr from './../images/mobile/image-interactive.jpg'
+
+const Interactive = () => {
+  return (
+    <section className='interactive' >
+      <img src={vr} alt="guy with vr on" />
+      <div className="content">
+        <h3>THE LEADER IN INTERACTIVE VR</h3>
+        <p>Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best companies around the globe. Our award-winning creations have transformed businesses through digital experiences that bind to their brand.</p>
+      </div>
+    </section >
+  )
+}
+
+export default Interactive
